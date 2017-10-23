1f0788f6b4bc17782d455a90b3506df244390bf8
hangjun zhao <alephman@gmail.com>
2017-10-23 20:07:18 +0800

    add a picture

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 记录学习scrapy爬虫
+
+https://github.com/vlon/scrapy/raw/master/dog.jpg
---
+++ links.py
@@ -0,0 +1,21 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import datetime
+import random
+import re
+
+random.seed(datetime.datetime.now())
+def getLinks(articleUrl):
+	html = urlopen("http://en.wikipedia.org" + articleUrl)
+	bs0bj = BeautifulSoup(html, "lxml")
+	return bs0bj.find("div", {"id":"bodyContent"}).findAll("a", href = re.compile("^(/wiki/)((?!:).)*$"))
+
+links = getLinks("/wiki/Kevin_Bacon")
+while len(links) > 0:
+	newArticle = links[random.randint(0, len(links) - 1)].attrs["href"]
+	if newArticle == "/wiki/Plato":
+		print("Get It!")
+		exit(0)
+	else:
+		print(newArticle)
+	links = getLinks(newArticle)
